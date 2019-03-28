e0cd5fb4d790e7496603b70bd5b65b26e5664592
Michael Chaffee <mchaffee@thoughtworks.com>
2019-03-28 05:50:07 -0400

    Refactor for expressivity

---
+++ README.md
@@ -0,0 +1,3 @@
+It's a little tricky to round the corners of a UIView AND add a drop shadow.
+
+This is a toy project containing a couple solutions to the challenge.
--- Rounded/ViewController.swift
+++ Rounded/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let defaultRadius: CGFloat = 16.0
 
     @IBOutlet weak var staticImageView: UIImageView?
     @IBOutlet weak var staticUnderview: UIView?
@@ -8,37 +9,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var dynamicImageView: UIImageView?
     private var dynamicUnderview: UIView?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setCornerRadius(on: staticImageView)
-        setCornerRadius(on: staticUnderview, clipping: false)
-        addDropShadow(to: staticUnderview)
+    override func viewDidLayoutSubviews() {
+        setDropShadowsOnStaticView()
+        setDropShadowsOnDynamicView()
     }
 
-    override func viewDidLayoutSubviews() {
-        guard dynamicUnderview == nil else { return }
+    func setDropShadowsOnStaticView() {
+        setCornerRadiusAndDropShadow(radius: self.defaultRadius, view: staticImageView, underview: staticUnderview)
+    }
+
+    func setDropShadowsOnDynamicView() {
+        guard let dynamicImageView = self.dynamicImageView,
+              self.dynamicUnderview == nil
+                else { return }
 
         dynamicUnderview = attachUnderview(to: dynamicImageView)
-        setCornerRadius(on: dynamicImageView)
-        setCornerRadius(on: dynamicUnderview, clipping: false)
-        addDropShadow(to: dynamicUnderview)
+
+        setCornerRadiusAndDropShadow(radius: defaultRadius, view: dynamicImageView, underview: dynamicUnderview)
     }
 
-    func setCornerRadius(on view: UIView?, clipping: Bool = true) {
-        guard let view = view else { return }
-        let layer = view.layer
-
-        layer.cornerRadius = 16.0
-        view.clipsToBounds = clipping
+    private func setCornerRadiusAndDropShadow(radius: CGFloat, view: UIImageView?, underview: UIView?) {
+        setCornerRadius(of: radius, on: view)
+        setCornerRadius(of: radius, on: underview, clipping: false)
+        addDropShadow(to: underview)
     }
 
     func attachUnderview(to view: UIView?) -> UIView? {
         guard let view = view,
-            let superview = view.superview
-            else { return nil }
+              let superview = view.superview
+                else { return nil }
 
         let underView = UIView(frame: view.frame)
+        // TODO make constraints work
         underView.translatesAutoresizingMaskIntoConstraints = false
 
         superview.insertSubview(underView, belowSubview: view)
@@ -46,13 +48,21 @@ class ViewController: UIViewController {
         return underView
     }
 
-    func addDropShadow(to view: UIView?) {
+    func setCornerRadius(of radius: CGFloat, on view: UIView?, clipping: Bool = true) {
         guard let view = view else { return }
         let layer = view.layer
 
+        layer.cornerRadius = radius
+        view.clipsToBounds = clipping
+    }
+
+    func addDropShadow(to view: UIView?) {
+        guard let view = view else { return }
+
+        let layer = view.layer
         layer.shadowPath =
-            UIBezierPath(roundedRect: view.bounds,
-                         cornerRadius: layer.cornerRadius).cgPath
+                UIBezierPath(roundedRect: view.bounds,
+                        cornerRadius: layer.cornerRadius).cgPath
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowOpacity = 0.5
         layer.shadowOffset = CGSize(width: 10, height: 10)
